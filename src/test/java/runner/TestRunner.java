88dab98c2fc9f94b006437c7b33c5164d8f36dc4
ak_learning <konijerla.anjana@gmail.com>
2019-05-11 04:10:05 +1200

    updated test runner

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,10 +1,16 @@
 package runner;
 
 
+import java.io.IOException;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
+import utility.BrowserInstance;
+import utility.PropertiesFileReader;
 
 
 
@@ -15,5 +21,17 @@ import cucumber.api.junit.Cucumber;
                 "json:target/cucumber.json", "pretty:target/cucumber-pretty.txt",
                 "usage:target/cucumber-usage.json", "junit:target/cucumber-results.xml","rerun:target/rerun.txt" }
                 )
-public class TestRunner{
+public class TestRunner extends BrowserInstance{
+	@BeforeClass
+	public static void setUP() throws IOException, InterruptedException {
+	driver = BrowserInstance.getBrowser("Chrome", 74, "windows");
+	driver.get(PropertiesFileReader.getProperty("browser.baseURL"));
+	Thread.sleep(3000);
+	}
+     @AfterClass
+     public static void tearDown(){
+
+     driver.quit();
+
+}
 }
