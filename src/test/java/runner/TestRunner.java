6b19f5b64b67ae80c0bbb6782628bba57f109ab2
ak_learning <konijerla.anjana@gmail.com>
2019-05-11 14:13:05 +1200

    genearted test reports

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,37 +1,36 @@
 package runner;
 
-
+import java.io.File;
 import java.io.IOException;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 
+import com.cucumber.listener.Reporter;
+
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import utility.BrowserInstance;
 import utility.PropertiesFileReader;
 
-
-
 @RunWith(Cucumber.class)
-@CucumberOptions(features= {"features"},
-        glue = {"stepDefinition"},
-        plugin = { "html:target/cucumber-html-report",
-                "json:target/cucumber.json", "pretty:target/cucumber-pretty.txt",
-                "usage:target/cucumber-usage.json", "junit:target/cucumber-results.xml","rerun:target/rerun.txt" }
-                )
-public class TestRunner extends BrowserInstance{
+@CucumberOptions(features = { "features" }, glue = { "stepDefinition" }, plugin = { "html:target/cucumber-html-report","com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/report.html",
+		"json:target/cucumber.json", "pretty:target/cucumber-pretty.txt", "usage:target/cucumber-usage.json",
+		"junit:target/cucumber-results.xml", "rerun:target/rerun.txt", "html:target/htmlreports" })
+public class TestRunner extends BrowserInstance {
 	@BeforeClass
 	public static void setUP() throws IOException, InterruptedException {
-	driver = BrowserInstance.getBrowser("Chrome", 74, "windows");
-	driver.get(PropertiesFileReader.getProperty("browser.baseURL"));
-	Thread.sleep(3000);
+		driver = BrowserInstance.getBrowser("Chrome", 74, "windows");
+		driver.get(PropertiesFileReader.getProperty("browser.baseURL"));
+		Thread.sleep(3000);
 	}
-     @AfterClass
-     public static void tearDown(){
 
-     driver.quit();
+	@AfterClass
+	public static void writeExtentReport() {
+		driver.quit();
+		Reporter.loadXMLConfig(new File("config/report.xml"));
+		
 
-}
+	}
 }
